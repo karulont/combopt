7b2463b809f4fd4c4fc30c37ae7fbcb7017b7671
Karl Tarbe <karulont@gmail.com>
2015-04-23 16:58:56 +0300

    Fix some errors and add some debug output

--- project6/project6.py
+++ project6/project6.py
@@ -4,47 +4,54 @@ import networkx as nx
 def find_foreground(m,n,P,k,a,b):
     g = nx.DiGraph()
 
+    sizeV = m*n
+    bprim = b / sizeV
+    aprim = a / sizeV
+    print ("aprim", aprim, "bprim", bprim)
+
     for x in range(m):
-        for y in range(m):
+        for y in range(n):
             for x_ in range( max(0,x-k), min(m,x+k+1) ):
                 for y_ in range(max(0,y-k), min(n,y+k+1)):
                     d2 = (x_-x)**2 + (y_-y)**2
                     if d2 == 0:
                         continue
                     c = k/d2
-                    g.add_edge((x,y), (x_,y_), capacity=c)
-                    g.add_edge((x_,y_), (x,y), capacity=c)
+                    print (x,y,c,c*aprim)
+                    g.add_edge((x,y), (x_,y_), capacity=c*aprim)
+                    g.add_edge((x_,y_), (x,y), capacity=c*aprim)
 
     g.add_node("s")
     g.add_node("t")
 
     for x in range(m):
-        for y in range(m):
-            g.add_edge("s", (x,y), capacity=P[x][y] * b)
-            g.add_edge((x,y), "t", capacity=P[x][y] * b)
+        for y in range(n):
+            g.add_edge("s", (x,y), capacity=P[x][y] * bprim)
+            g.add_edge((x,y), "t", capacity=(1-P[x][y]) * bprim)
+
+    print(str(g.edges(data=True)))
 
     (value,partition) = nx.minimum_cut(g,"s","t")
     reachable, non_reachable = partition
-    #print("Non-reachable: " + str(non_reachable))
-    #print("Reachable: " + str(reachable))
+    print("Non-reachable: " + str(non_reachable))
+    print("Reachable: " + str(reachable))
 
-    F=[[0 for i in range(m)] for j in range(n)]
+    F=[[0 for i in range(n)] for j in range(m)]
 
-    for x in range(m):
-        for y in range(m):
-            if (x,y) in reachable:
-                F[x][y] = 1
+    reachable.remove('s')
+    for (x,y) in reachable:
+        F[x][y] = 1
 
     return F
 
-P = read_instance_from_file("v2_test-00.img")
+P = read_instance_from_file("v2_test-0.img")
 m = len(P)
 n = len(P[0])
 print("m: " + str(m))
 print("n: " + str(n))
 
 k = 2
-a = 1
+a = 0
 b = 1
 
 F = find_foreground(m,n,P,k,a,b)
