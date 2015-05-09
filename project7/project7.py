ed290a561933e75e127e4f4a8042bb4d42cbea1c
Sander Soo <sander92@ut.ee>
2015-05-09 23:58:05 +0300

    project7 possible fix

--- project7/project7.py
+++ project7/project7.py
@@ -30,6 +30,7 @@ def solve(snapshots):
   print('Solving')
   n = len(snapshots)
 
+
   perms = []
   for i in range(0, n - 1):
     perm = best_assignment(snapshots[i], snapshots[i + 1])
@@ -39,7 +40,18 @@ def solve(snapshots):
     #print(perm)
     print('Distance sum:', sum([dist(u, v) for u, v in perm.items()]))
 
-  return None
+  last_perm = [i for i in range(len(snapshots[0]))]
+  newperms=[n]
+  for i,perm in enumerate(perms):
+    newperm = [0] * len(snapshots[0])
+    for v1,v2 in perm.items():
+      v1i=snapshots[i].index(v1)
+      v2i=snapshots[i+1].index(v2)
+      newperm[v2i] = last_perm[v1i]
+    newperms+=[newperm]
+  # import drawful
+  # drawful.drawWithIndices(snapshots, newperms[1], newperms[2])
+  return newperms
 
 def read_instance_from_file(file):
   print('Reading input from', file)
@@ -49,14 +61,21 @@ def read_instance_from_file(file):
     return [[tuple(u) for u in ss[1]] for ss in snapshots]
 
 
+
 def write_output(file, solution):
   with open(file, 'w') as f:
     json.dump(solution, f)
   print('Saved solution to', file)
 
+def read_output(file):
+  print('Reading output from', file)
+  with open(file) as f:
+    n,pi0, pi1, pi2 = json.load(f)
+    #snapshots.sort(key = lambda ss: ss[0])
+    return [n, pi1, pi2]
 
 def main():
-  input_file = argv[1] if len(argv) >= 2 else 'example-points.lst'
+  input_file = argv[1] if len(argv) >= 2 else 'points-00125-0.lst'
   snapshots = read_instance_from_file(input_file)
   solution = solve(snapshots)
   if solution:
